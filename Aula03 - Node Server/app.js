901902c8d83f725cef9f94cf54dd36082e57be68
unknown <emillyn_muniz@hotmail.com>
2020-07-07 18:47:37 -0300

    Curso NodeJs

--- Aula03 - Node Server/app.js
+++ Aula03 - Node Server/app.js
@@ -21,19 +21,19 @@ const server = http.createServer((req, res) => {
             body.push(chunck);
         });
 
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end()
     }
     // process.exit();
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html');
+    res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js</h1></body>');
     res.write('</html>');
