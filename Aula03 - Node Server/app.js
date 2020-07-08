5255d97b16d89745c8e0ea80e9e35996b2387b34
unknown <emillyn_muniz@hotmail.com>
2020-07-08 01:47:26 -0300

    Curso NodeJs

--- Aula03 - Node Server/app.js
+++ Aula03 - Node Server/app.js
@@ -1,44 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    // console.log(req.url, req.method, req.headers)
-    if(url === '/'){
-        res.write('<html');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message" placeholder="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-
-        req.on('data', (chunck) => {
-            console.log(chunck)
-            body.push(chunck);
-        });
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-    // process.exit();
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js</h1></body>');
-    res.write('</html>');
-    res.end();
-});
-
+const server = http.createServer(routes.handler);
+console.log(routes.someText);
 
 server.listen(3000);
